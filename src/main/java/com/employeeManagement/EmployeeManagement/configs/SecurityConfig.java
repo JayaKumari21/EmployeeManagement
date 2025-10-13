56bd343823054b7e54841efab91066c916a282c5
Jaya Kumari <jayakumarijk8540@gmail.com>
2025-10-14 01:05:47 +0530

    Added security dependecy and did basic auth

--- src/main/java/com/employeeManagement/EmployeeManagement/configs/SecurityConfig.java
+++ src/main/java/com/employeeManagement/EmployeeManagement/configs/SecurityConfig.java
@@ -1,10 +1,39 @@
-//package com.employeeManagement.EmployeeManagement.configs;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
-//}
+package com.employeeManagement.EmployeeManagement.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/swagger-resources/**",
+                                "/webjars/**",
+                                "/actuator/**",
+                                "/public/**"
+                        )
+                        .permitAll()
+                        .anyRequest().authenticated()
+                )
+
+//                .formLogin(Customizer.withDefaults());
+                
+                .httpBasic(Customizer.withDefaults());
+
+
+        return httpSecurity.build();
+    }
+}
